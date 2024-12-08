ad628baa9285a863975051dd3a3682fe7ef3fbe0
batuwankenobi <batuhanbartuu@gmail.com>
2024-12-08 03:24:18 +0300

    atm
    1-View Balance
        "2-Withdraw Money"
        "3-Deposit Money"
        "4-Exit"
        "Please select a value.";

---
+++ atm.js/atm.js
@@ -0,0 +1,16 @@
+/*
+1-View balance
+2-Withdraw money
+3-Deposit money
+4-Exit
+
+ATM APPLICATION
+*/
+let newLine = "\r\n";
+let balance = 1000;
+
+let text = "1-View Balance " + newLine +
+    "2-Withdraw Money" + newLine +
+    "3-Deposit Money" + newLine +
+    "4-Exit" + newLine +
+    "Please select a value.";
